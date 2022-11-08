574b0b1c07ee435c0bd323ef66324cbad39f0634
ruby1208 <ng.p.l.ruby@gmail.com>
2022-11-08 19:15:33 +0000

    Removed failed import for PDFs

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,17 +1,11 @@
 import { NextPage } from "next"
 import AboutMe from "../components/about-me"
 import Container from "../components/container"
-import ReactPDF from '@react-pdf/renderer'
 
 const CV: NextPage = () => {
   return (
     <>
       <Container>    
-        <ReactPDF
-          file = {{
-            url: '/CV_Ruby_Ng.pdf'
-          }}>
-        </ReactPDF>
         <AboutMe/>
 
       </Container>
